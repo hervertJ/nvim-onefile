6f53078fdcefcdb7f836d427c0818e5bbaaf9bd5
hervertJ <hervertjimenez1@gmail.com>
2026-08-03 14:07:51 -0500

    feat: gruvbox colorscheme

--- init.lua
+++ init.lua
@@ -30,18 +30,19 @@ vim.keymap.set('n', '<leader>q', ':quit<CR>')
 --==================================================
 
 vim.pack.add({
-	{ src = "https://github.com/folke/tokyonight.nvim.git" },
+  { src = "https://github.com/folke/tokyonight.nvim.git" },
+  { src = "https://github.com/ellisonleao/gruvbox.nvim" },
 
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim.git" },
-	{ src = "https://github.com/saghen/blink.lib" },
-	{ src = "https://github.com/saghen/blink.cmp" },
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	{ src = "https://github.com/nvim-mini/mini.nvim" },
-	{ src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
-	{ src = "https://github.com/folke/snacks.nvim.git" },
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+  { src = "https://github.com/mason-org/mason.nvim.git" },
+  { src = "https://github.com/saghen/blink.lib" },
+  { src = "https://github.com/saghen/blink.cmp" },
+  { src = "https://github.com/stevearc/oil.nvim" },
+  { src = "https://github.com/nvim-mini/mini.nvim" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter.git" },
+  { src = "https://github.com/folke/snacks.nvim.git" },
 
-	{ src = "https://github.com/chomosuke/typst-preview.nvim.git" },
+  { src = "https://github.com/chomosuke/typst-preview.nvim.git" },
 })
 
 --==================================================
@@ -52,11 +53,11 @@ vim.pack.add({
 vim.lsp.enable({ "lua_ls", "tinymist", "clangd", "pyright", "nil_ls" })
 vim.keymap.set('n', '<leader>uf', vim.lsp.buf.format)
 vim.keymap.set('n', '<leader>ud', function() --on/off warnigns
-	if vim.diagnostic.is_enabled() then
-		vim.diagnostic.enable(false)
-	else
-		vim.diagnostic.enable(true)
-	end
+  if vim.diagnostic.is_enabled() then
+    vim.diagnostic.enable(false)
+  else
+    vim.diagnostic.enable(true)
+  end
 end, { desc = "Toggle Diagnostics" })
 
 -- MASON
@@ -65,38 +66,39 @@ require('mason').setup()
 -- BLINK-CMP
 local cmp = require('blink.cmp')
 cmp.setup({
-	keymap = {
-		['<CR>'] = { 'accept', 'fallback', },
-	},
+  keymap = {
+    ['<CR>'] = { 'accept', 'fallback', },
+  },
 })
 cmp.build():pwait()
 cmp.setup()
 
 -- OIL
 require('oil').setup({
-	columns = {
-		"permissions",
-		"size",
-		"mtime",
-	},
+  columns = {
+    "permissions",
+    "size",
+    "mtime",
+  },
 })
 vim.keymap.set('n', '-', ":Oil<CR>")
 
 -- MINI.NVIM
 require('mini.pairs').setup()
 require('mini.surround').setup()
+require('mini.cursorword').setup()
 
 -- TREESITTER
 require('nvim-treesitter').setup()
 require('nvim-treesitter').install { 'bash', 'python', 'c', 'lua', 'nix' }
 vim.api.nvim_create_autocmd('FileType', {
-	pattern = { 'bash', 'python', 'c', 'lua', 'nix' },
-	callback = function() vim.treesitter.start() end,
+  pattern = { 'bash', 'python', 'c', 'lua', 'nix' },
+  callback = function() vim.treesitter.start() end,
 })
 
 -- SNACKS
 require('snacks').setup({
-	picker = { enabled = true },
+  picker = { enabled = true },
 })
 vim.keymap.set('n', '<leader><space>', ":lua Snacks.picker.files()<CR>")
 vim.keymap.set('n', '<leader>/', ":lua Snacks.picker.grep()<CR>")
@@ -106,6 +108,33 @@ vim.keymap.set('n', '<leader>/', ":lua Snacks.picker.grep()<CR>")
 --==================================================
 
 require("tokyonight").setup()
-vim.cmd.colorscheme("tokyonight-night")
+
+require("gruvbox").setup({
+  terminal_colors = true,
+  bold = false,
+
+  overrides = {
+    SignColumn = { bg = "#282828" },
+    DiagnosticSignWarn = { fg = "#fabd2f", bg = "#282828" },
+    DiagnosticSignError = { fg = "#fb4934", bg = "#282828" },
+    ["@lsp.type.function.lua"] = { fg = "#fe8019" },
+  },
+})
+
+vim.cmd.colorscheme("gruvbox")
 
 vim.cmd(":hi statusline guibg=NONE")
+
+
+
+
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "lua", "qml" },
+  callback = function()
+    vim.bo.shiftwidth = 2
+    vim.bo.tabstop = 2
+    vim.bo.softtabstop = 2
+    vim.bo.expandtab = true
+  end,
+})
